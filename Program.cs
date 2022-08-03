739cf31795c489922282e08682b90c43253cd688
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-08-03 15:32:09 -0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+class program{
+    static void Main(string[] args)
+    {
+        // Tipo de Referencia
+        // -> Endereço dos dados
+        var pagamentoBoleto = new PagamentoBoleto();
+        pagamentoBoleto.Pagar();
+        pagamentoBoleto.Vencimento = DateTime.Now;
+        pagamentoBoleto.NumeroBoleto = "123";
+
+        var pagemento = new Pagamento();
+    }
+
+}
+
+class Pagamento
+{
+
+    public DateTime Vencimento;
+
+    public void Pagar(){
+        
+    }
+
+}
+
+class PagamentoBoleto : Pagamento
+{
+    public string NumeroBoleto;
+
+}
+class PagamentoCredito : Pagamento
+{
+    public string Numero; 
+}
